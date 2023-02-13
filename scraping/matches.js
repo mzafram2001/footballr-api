03d6c7aa55a56b615b0951cd283eb31b5b9f2b3c
Miguel Zafra <mzafram2001@gmail.com>
2023-02-13 19:27:23 +0100

    UPDATE Scraping Files

---
+++ scraping/matches.js
@@ -0,0 +1,160 @@
+const PUPPETER = require('puppeteer');
+const FS = require('fs');
+const PATH = require('path');
+
+// // // // // // // // // // URLs // // // // // // // // // //
+const URLS = {
+    spain_matches_2021: "https://www.flashscore.com/football/spain/laliga-2021-2022/results/",
+    spain_matches_2022: "https://www.flashscore.com/football/spain/laliga/results/",
+}
+
+// // // // // // // // // // CODE MATCHES // // // // // // // // // //
+async function getMatches(url) {
+    const BROWSER = await PUPPETER.launch({
+        headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const PAGE = await BROWSER.newPage();
+    await PAGE.goto(url, { waitUntil: "domcontentloaded" });
+    // REPETIR EL SIGUIENTE CODIGO TANTAS VECES COMO BOTON DE MOSTRAR MÁS PARTIDOS HAYA
+    await PAGE.waitForSelector('.event__more', { visible: true });
+    await PAGE.evaluate(() => {
+        document.querySelector('.event__more').click();
+    });
+    await delay(4000);
+    await PAGE.waitForSelector('.event__more', { visible: true });
+    await PAGE.evaluate(() => {
+        document.querySelector('.event__more').click();
+    });
+    await delay(4000);
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    const RESULT = await PAGE.evaluate(() => {
+        const JSON = {};
+        JSON.name = document.querySelector('#mc > div.container__livetable > div.container__heading > div.heading > div.heading__title > div.heading__name').innerText;
+        switch (JSON.name) {
+            case "LaLiga": JSON.area = "ESP";
+                break;
+            case "Bundesliga": JSON.area = "GER";
+                break;
+            case "Serie A": JSON.area = "ITA";
+                break;
+            case "Ligue 1": JSON.area = "FRA";
+                break;
+            case "Premier League": JSON.area = "ENG";
+                break;
+        };
+        JSON.yearStart = document.querySelector('#mc > div.container__livetable > div.container__heading > div.heading > div.heading__info').innerText;
+        JSON.yearStart = parseInt(JSON.yearStart.substring(0, 4));
+        JSON.yearEnd = document.querySelector('#mc > div.container__livetable > div.container__heading > div.heading > div.heading__info').innerText;
+        JSON.yearEnd = parseInt(JSON.yearEnd.substring(5, JSON.yearStart - 1));
+        JSON.season = [];
+        JSON.matchesIteration = [];
+        const ROUNDS_SELECTOR = document.querySelectorAll('.event__round');
+        const MATCHES_SELECTOR = document.querySelectorAll('.event__match');
+        var round = 0;
+        // Creo un array de objetos con todos los partidos
+        for (var i = MATCHES_SELECTOR.length - 1; i >= 0; i--) {
+            const TMP2 = {};
+            TMP2.id = MATCHES_SELECTOR[i].id.substring(4);
+            TMP2.link = "https://www.flashscore.com/match/" + TMP2.id;
+            JSON.matchesIteration.push(TMP2);
+        }
+        // Meto en el JSON todas las rounds sin repetirlas
+        for (var i = ROUNDS_SELECTOR.length - 1; i >= 0; i--) {
+            const TMP = {};
+            var found = false;
+            TMP.round = parseInt(ROUNDS_SELECTOR[i].innerText.substring(6));
+            round = parseInt(TMP.round);
+            TMP.matches = [];
+            for (index in JSON.season) {
+                if (JSON.season[index].round == TMP.round) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                JSON.season.push(TMP);
+            }
+        }
+        return JSON;
+    });
+
+    for (let match of RESULT.matchesIteration) {
+        await PAGE.goto(match.link);
+        console.log(match.link);
+
+        const MATCH = await PAGE.evaluate(() => {
+            const TMP = {};
+            var dumpString;
+            var dumpStringArray;
+
+            TMP.homeTeam = {};
+            dumpString = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__home > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow > a').getAttribute('href');
+            dumpStringArray = dumpString.split('/');
+            TMP.homeTeam.id = dumpStringArray[3];
+            TMP.homeTeam.name = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__home > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow').innerText;
+            TMP.homeTeam.logo = "https://raw.githubusercontent.com/mzafram2001/zeus-src/main/static/logos/" + TMP.homeTeam.id + "_logo.png";
+            TMP.homeTeam.kit = "https://raw.githubusercontent.com/mzafram2001/zeus-src/main/static/kits/" + TMP.homeTeam.id + "_kit.png";
+
+            TMP.awayTeam = {};
+            dumpString = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__away > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow > a').getAttribute('href');
+            dumpStringArray = dumpString.split('/');
+            TMP.awayTeam.id = dumpStringArray[3];
+            TMP.awayTeam.name = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__away > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow').innerText;
+            TMP.awayTeam.logo = "https://raw.githubusercontent.com/mzafram2001/zeus-src/main/static/logos/" + TMP.awayTeam.id + "_logo.png";
+            TMP.awayTeam.kit = "https://raw.githubusercontent.com/mzafram2001/zeus-src/main/static/kits/" + TMP.awayTeam.id + "_kit.png";
+            
+            TMP.round = parseInt(document.querySelector('#detail > div.tournamentHeader.tournamentHeaderDescription > div > span.tournamentHeader__country > a').innerText.substring(15));
+            TMP.date = document.querySelector('.duelParticipant__startTime').innerText.substring(0, 10);
+            TMP.hour = document.querySelector('.duelParticipant__startTime').innerText.substring(11);
+            TMP.home = document.querySelector('.duelParticipant__home').innerText;
+            TMP.away = document.querySelector('.duelParticipant__away').innerText;
+            TMP.homeGoals = parseInt(document.querySelector('#detail > div.duelParticipant > div.duelParticipant__score > div > div.detailScore__wrapper > span:nth-child(1)').innerText);
+            TMP.awayGoals = parseInt(document.querySelector('#detail > div.duelParticipant > div.duelParticipant__score > div > div.detailScore__wrapper > span:nth-child(3)').innerText);
+            TMP.status = document.querySelector('.fixedHeaderDuel__detailStatus').innerText;
+            return TMP;
+        });
+        match.round = MATCH.round;
+        match.date = MATCH.date;
+        match.hour = MATCH.hour;
+        match.homeTeam = MATCH.homeTeam;
+        match.awayTeam = MATCH.awayTeam;
+        match.homeGoals = MATCH.homeGoals;
+        match.awayGoals = MATCH.awayGoals;
+        match.status = MATCH.status;
+    }
+
+    switch (RESULT.name) {
+        case "LaLiga": var fileLocation = PATH.join(process.cwd(), "./db/" /*+ RESULT.yearStart + */ + "/matchesLaLiga" + RESULT.yearStart + "Flashcore.json");
+            break;
+        case "Primera Division": var fileLocation = PATH.join(process.cwd(), "./db/" + RESULT.yearStart + "/matchesLaLiga" + RESULT.yearStart + "Flashcore.json");
+            break;
+        case "Bundesliga": var fileLocation = PATH.join(process.cwd(), "./db/" + RESULT.yearStart + "/matchesBundesliga" + RESULT.yearStart + "Flashcore.json");
+            break;
+        case "Serie A": var fileLocation = PATH.join(process.cwd(), "./db/" + RESULT.yearStart + "/matchesSerieA" + RESULT.yearStart + "Flashcore.json");
+            break;
+        case "Ligue 1": var fileLocation = PATH.join(process.cwd(), "./db/" + RESULT.yearStart + "/matchesLigue1" + RESULT.yearStart + "Flashcore.json");
+            break;
+        case "Premier League": var fileLocation = PATH.join(process.cwd(), "./db/" + RESULT.yearStart + "/matchesPremierLeague" + RESULT.yearStart + "Flashcore.json");
+            break;
+    }
+
+    FS.writeFile(fileLocation, JSON.stringify(RESULT), 'utf8', function (err) {
+        if (err) {
+            console.log('An error occured while writing JSON Object to File.');
+            return console.log(err);
+        }
+        console.log('JSON file has been saved.');
+    });
+    await BROWSER.close();
+}
+
+// // // // // // // // // // DELAY STANDINGS // // // // // // // // // //
+async function delay(time) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, time)
+    });
+}
+
+// // // // // // // // // // FUNCTION CALL // // // // // // // // // //
+getMatches(URLS.spain_matches_2022);

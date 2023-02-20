1c4866049ca256003e14d927c274cc984493b5e8
Miguel Zafra <mzafram2001@gmail.com>
2023-02-20 19:03:59 +0100

    UPDATE

--- scraping/matches.js
+++ scraping/matches.js
@@ -576,4 +576,4 @@ async function delay(time) {
 }
 
 // // // // // // // // // // FUNCTION CALL // // // // // // // // // //
-getMatches(URLS.spain_matches_2022);
+getMatches(URLS.spain_matches_2019);

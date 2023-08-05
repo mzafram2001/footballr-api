3fdc8362be8ac37b7704ec6c9290de534024a7f3
Miguel Zafra <mzafram2001@gmail.com>
2023-08-05 11:31:30 +0200

    UPDATE Index API

--- api/index.js
+++ api/index.js
@@ -117,7 +117,7 @@ APP.get('/', (ctx) => {
 
 	const zeusAPI = {
 		name: "Zeus API ⚡",
-		version: 'v1.14',
+		version: 'v1.20',
 		updated: `${day}.${month}.${year} ${hours}:${minutes}`,
 		message: 'Created with 💙 by Miguel Zafra.'
 	};

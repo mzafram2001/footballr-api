a7c8a0e53e0a4b4b5d8e2850e85fe434d5d02b7b
Miguel Zafra <mzafram2001@gmail.com>
2023-08-12 21:04:34 +0200

    UPDATE Matches Results

--- scraping/matchesLaLiga.js
+++ scraping/matchesLaLiga.js
@@ -16,25 +16,186 @@ async function getLast10Matches(url) {
     });
     const PAGE = await BROWSER.newPage();
     await PAGE.goto(url, { waitUntil: "networkidle0" });
+    var original = require('D:/Proyectos/olympus/zeus-api/db/2023/matches/matchesLaLiga2023Flashcore.json');
     const RESULT = await PAGE.evaluate(() => {
         const JSON = {};
+        JSON.season = [];
         JSON.matchesIteration = [];
+        const ROUNDS_SELECTOR = document.querySelectorAll('.event__round');
         const MATCHES_SELECTOR = document.querySelectorAll('.event__match');
+        var round = 0;
         var ok = Array.prototype.slice.call(MATCHES_SELECTOR);
         const LAST_10 = ok.slice(0, 10);
+        ok = Array.prototype.slice.call(ROUNDS_SELECTOR);
+        const ROUNDS_ARRAY = ok;
         for (var i = LAST_10.length - 1; i >= 0; i--) {
             const TMP = {};
             TMP.id = LAST_10[i].id.substring(4);
             TMP.link = "https://www.flashscore.com/match/" + TMP.id;
             JSON.matchesIteration.push(TMP);
         }
+        for (var i = ROUNDS_ARRAY.length - 1; i >= 0; i--) {
+            const TMP = {};
+            var found = false;
+            TMP.round = parseInt(ROUNDS_ARRAY[i].innerText.substring(6));
+            round = parseInt(TMP.round);
+            TMP.matches = [];
+            for (index in JSON.season) {
+                if (JSON.season[index].round == TMP.round) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                JSON.season.push(TMP);
+            }
+        }
         return JSON;
     });
-    var original = require('D:/Proyectos/olympus/zeus-api/db/2023/matches/matchesLaLiga2023Flashcore.json');
+
+    console.log(RESULT.season);
+    for (let match of RESULT.matchesIteration) {
+        await PAGE.goto(match.link, { 'waitUntil': 'networkidle0' });
+        console.log(match.link);
+        const MATCH = await PAGE.evaluate(() => {
+            const TMP = {};
+            var dumpString;
+            var dumpStringArray;
+            var title = document.evaluate("/html/head/title", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            TMP.homeTeam = {};
+            dumpString = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__home > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow > a').getAttribute('href');
+            dumpStringArray = dumpString.split('/');
+            TMP.homeTeam.id = dumpStringArray[3];
+            TMP.homeTeam.name = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__home > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow').innerText;
+            switch (TMP.homeTeam.name) {
+                case "Atl. Madrid": TMP.homeTeam.name = "Atlético Madrid";
+                    break;
+                case "Betis": TMP.homeTeam.name = "Real Betis";
+                    break;
+                case "Granada CF": TMP.homeTeam.name = "Granada";
+                    break;
+                case "Ath Bilbao": TMP.homeTeam.name = "Athletic Bilbao";
+                    break;
+                case "Cadiz CF": TMP.homeTeam.name = "Cádiz";
+                    break;
+                case "Alaves": TMP.homeTeam.name = "Deportivo Alavés";
+                    break;
+                case "Almeria": TMP.homeTeam.name = "Almería";
+                    break;
+                case "Malaga": TMP.homeTeam.name = "Málaga";
+                    break;
+                case "Dep. La Coruna": TMP.homeTeam.name = "Deportivo La Coruña";
+                    break;
+                case "Gijon": TMP.homeTeam.name = "Gijón";
+                    break;
+                case "Leganes": TMP.homeTeam.name = "Leganés";
+                    break;
+            }
+            TMP.homeTeam.shorthand = title.innerText.substring(0, 3);
+            TMP.homeTeam.logo = "https://raw.githubusercontent.com/mzafram2001/zeus-src/main/static/teams/" + TMP.homeTeam.id + ".svg";
+            TMP.awayTeam = {};
+            dumpString = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__away > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow > a').getAttribute('href');
+            dumpStringArray = dumpString.split('/');
+            TMP.awayTeam.id = dumpStringArray[3];
+            TMP.awayTeam.name = document.querySelector('#detail > div.duelParticipant > div.duelParticipant__away > div.participant__participantNameWrapper > div.participant__participantName.participant__overflow').innerText;
+            switch (TMP.awayTeam.name) {
+                case "Atl. Madrid": TMP.awayTeam.name = "Atlético Madrid";
+                    break;
+                case "Betis": TMP.awayTeam.name = "Real Betis";
+                    break;
+                case "Granada CF": TMP.awayTeam.name = "Granada";
+                    break;
+                case "Ath Bilbao": TMP.awayTeam.name = "Athletic Bilbao";
+                    break;
+                case "Cadiz CF": TMP.awayTeam.name = "Cádiz";
+                    break;
+                case "Alaves": TMP.awayTeam.name = "Deportivo Alavés";
+                    break;
+                case "Almeria": TMP.awayTeam.name = "Almería";
+                    break;
+                case "Malaga": TMP.awayTeam.name = "Málaga";
+                    break;
+                case "Dep. La Coruna": TMP.awayTeam.name = "Deportivo La Coruña";
+                    break;
+                case "Gijon": TMP.awayTeam.name = "Gijón";
+                    break;
+                case "Leganes": TMP.awayTeam.name = "Leganés";
+                    break;
+            }
+            TMP.awayTeam.shorthand = title.innerText.substring(8, 11);
+            TMP.awayTeam.logo = "https://raw.githubusercontent.com/mzafram2001/zeus-src/main/static/teams/" + TMP.awayTeam.id + ".svg";
+            dumpString = document.querySelector('#detail > div.tournamentHeader.tournamentHeaderDescription > div > span.tournamentHeader__country > a').innerText;
+            dumpStringArray = dumpString.split(" ");
+            TMP.round = parseInt(dumpStringArray[dumpStringArray.length - 1]);
+            if (TMP.round == null) {
+                TMP.round = "Relegation Play-Offs";
+            }
+            TMP.date = document.querySelector('.duelParticipant__startTime').innerText.substring(0, 10);
+            TMP.hour = document.querySelector('.duelParticipant__startTime').innerText.substring(11);
+            TMP.home = document.querySelector('.duelParticipant__home').innerText;
+            TMP.away = document.querySelector('.duelParticipant__away').innerText;
+            TMP.homeGoals = parseInt(document.querySelector('#detail > div.duelParticipant > div.duelParticipant__score > div > div.detailScore__wrapper > span:nth-child(1)').innerText);
+            TMP.awayGoals = parseInt(document.querySelector('#detail > div.duelParticipant > div.duelParticipant__score > div > div.detailScore__wrapper > span:nth-child(3)').innerText);
+            TMP.status = document.querySelector('.fixedHeaderDuel__detailStatus').innerText;
+            TMP.stats = [];
+            return TMP;
+        });
+        match.round = MATCH.round;
+        match.date = MATCH.date;
+        match.hour = MATCH.hour;
+        match.homeTeam = MATCH.homeTeam;
+        match.awayTeam = MATCH.awayTeam;
+        match.homeGoals = MATCH.homeGoals;
+        match.awayGoals = MATCH.awayGoals;
+        match.status = MATCH.status;
+        match.stats = MATCH.stats;
+    }
+
+    for (let match of RESULT.matchesIteration) {
+        await PAGE.goto(match.link + "/#/match-summary/match-statistics/0", { 'waitUntil': 'networkidle0' });
+        console.log(match.link + "/#/match-summary/match-statistics/0");
+        await delay(1000);
+        const MATCH_STATS = await PAGE.evaluate(() => {
+            const TMP = {};
+            const STAT_ROWS = document.querySelectorAll('.stat__row');
+            STAT_ROWS.forEach(element => {
+                categoryName = element.querySelector('.stat__categoryName').innerText;
+                switch (categoryName) {
+                    case "Ball Possession":
+                        TMP.ballPossessionHome = element.querySelector('.stat__homeValue').innerText;
+                        TMP.ballPossessionAway = element.querySelector('.stat__awayValue').innerText;
+                        break;
+                    case "Goal Attempts":
+                        TMP.goalAttemptsHome = parseInt(element.querySelector('.stat__homeValue').innerText);
+                        TMP.goalAttemptsAway = parseInt(element.querySelector('.stat__awayValue').innerText);
+                        break;
+                    case "Offsides":
+                        TMP.offsidesHome = parseInt(element.querySelector('.stat__homeValue').innerText);
+                        TMP.offsidesAway = parseInt(element.querySelector('.stat__awayValue').innerText);
+                        break;
+                    case "Fouls":
+                        TMP.foulsHome = parseInt(element.querySelector('.stat__homeValue').innerText);
+                        TMP.foulsAway = parseInt(element.querySelector('.stat__awayValue').innerText);
+                        break;
+                    case "Total Passes":
+                        TMP.totalPassesHome = parseInt(element.querySelector('.stat__homeValue').innerText);
+                        TMP.totalPassesAway = parseInt(element.querySelector('.stat__awayValue').innerText);
+                        break;
+                    case "Attacks":
+                        TMP.attacksHome = parseInt(element.querySelector('.stat__homeValue').innerText);
+                        TMP.attacksAway = parseInt(element.querySelector('.stat__awayValue').innerText);
+                        break;
+                }
+            });
+            return TMP;
+        });
+        match.stats = MATCH_STATS;
+    }
+    original.season = RESULT.season;
     original.matchesIteration = RESULT.matchesIteration;
     console.log(original);
-    FS.truncate('D:/Proyectos/olympus/zeus-api/db/2023/matches/matchesLaLiga2023Flashcore.json', 0, function(err){
-        if(err) {
+    FS.truncate('D:/Proyectos/olympus/zeus-api/db/2023/matches/matchesLaLiga2023Flashcore.json', 0, function (err) {
+        if (err) {
             console.log('An error occurred while truncating the file.');
             return console.log(err);
         }
@@ -53,7 +214,6 @@ async function getLast10Matches(url) {
         }
     }
     delete original.matchesIteration;
-    console.log(original);
     FS.writeFile('D:/Proyectos/olympus/zeus-api/db/2023/matches/matchesLaLiga2023Flashcore.json', JSON.stringify(original), function (err) {
         if (err) {
             console.log('An error occured while writing JSON Object to File.');

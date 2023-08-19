56f50bf3e24482fc814b5f496218b9da41626ece
Miguel Zafra <mzafram2001@gmail.com>
2023-08-19 10:35:15 +0200

    UPDATE Matches scripts and results

--- scraping/matchesBundesliga.js
+++ scraping/matchesBundesliga.js
@@ -19,21 +19,11 @@ async function getAllMatches(url) {
     const PAGE = await BROWSER.newPage();
     await PAGE.goto(url, { waitUntil: "networkidle0" });
     // // // // // REPETIR EL SIGUIENTE CODIGO TANTAS VECES COMO BOTON DE MOSTRAR MÁS PARTIDOS HAYA
-    await PAGE.waitForSelector('.event__more', { visible: true });
+    /*await PAGE.waitForSelector('.event__more', { visible: true });
     await PAGE.evaluate(() => {
         document.querySelector('.event__more').click();
     });
-    await delay(4000);
-    await PAGE.waitForSelector('.event__more', { visible: true });
-    await PAGE.evaluate(() => {
-        document.querySelector('.event__more').click();
-    });
-    await delay(4000);
-    await PAGE.waitForSelector('.event__more', { visible: true });
-    await PAGE.evaluate(() => {
-        document.querySelector('.event__more').click();
-    });
-    await delay(4000);
+    await delay(4000);*/
     /////////////////////////////////////////////////////////////////////////////////////
 
     const RESULT = await PAGE.evaluate(() => {

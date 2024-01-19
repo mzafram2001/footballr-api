2eb519dea4e59d0044607631d6a0b811aed92193
Miguel Zafra <mzafram2001@gmail.com>
2024-01-19 18:53:47 +0100

    UPDATE

--- scraping/standings.js
+++ scraping/standings.js
@@ -40,7 +40,7 @@ async function getStandings(url) {
         const HEADING_INFO = HEADING.querySelector('div.heading__info').innerText;
         JSON.yearStart = parseInt(HEADING_INFO.substring(0, 4));
         JSON.yearEnd = parseInt(HEADING_INFO.substring(5, JSON.yearStart - 1));
-        JSON.standsings = [];
+        JSON.standings = [];
         var numRow = 0;
         var dumpString;
         var dumpStringArray;

8e7f24d15a23b7c2ef06246259fc4e48897aa012
Miguel Zafra <mzafram2001@gmail.com>
2024-08-16 12:58:58 +0200

    UPDATE teams js

--- scraping/teams.js
+++ scraping/teams.js
@@ -110,7 +110,7 @@ async function getTeamsDetailedInfo(page, teams, teamsData) {
             return {
                 name: nameElement ? nameElement.innerText.trim() : '',
                 url: nameElement ? nameElement.href : '',
-                marketValue: valueElement ? valueElement.innerText.trim() : ''
+                marketValu: valueElement ? valueElement.innerText.trim() : ''
             };
         });
     });
